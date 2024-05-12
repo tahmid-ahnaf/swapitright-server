6d3958438f42322c206e128d618b7ee4834aa689
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2024-05-12 17:01:33 +0600

    post queries done

--- index.js
+++ index.js
@@ -33,14 +33,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const itemCollection = client.db("alternativeproductDB").collection("queries");
+    const queryCollection = client.db("alternativeproductDB").collection("queries");
     
 
     app.get("/queries", async (req, res) => {
-      const cursor = itemCollection.find();
+      const cursor = queryCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.post("/queries", async (req, res) => {
+      const newQuery = req.body;
+      const result = await queryCollection.insertOne(newQuery);
+      res.send(result);
+    });
+
 
 
     
