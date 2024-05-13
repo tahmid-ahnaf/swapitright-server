3f3a3e3a0fdee5783d663c0524b048137c51086b
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2024-05-14 02:09:50 +0600

    cookieoptions updated with access verification

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -8,9 +9,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
   origin: [
-    'http://localhost:5173/'
+    'http://localhost:5173'
   ],
   credentials: true
 }));
@@ -34,6 +36,28 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyToken = (req,res,next) =>{
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware', token);
+  if(!token){
+    return res.status(401).send({message:'unauthorized access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+    if(err)
+      {
+        return res.status(401).send({message:'unauthorized access'})
+      }
+      req.user = decoded;
+      next();
+  })
+}
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -49,17 +73,14 @@ async function run() {
       const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
         expiresIn: '1h'
       });
-      res.cookie('token', token, {
-        httpOnly:true,
-        secure: true,
-        sameSite:'none'
-      }).send({success: true});
+      res.cookie('token', token,cookieOptions).send({success: true});
     })
 
     app.post("/logout", async(req,res)=>{
       const user = req.body;
-      res.clearCookie('token', {maxAge:0}).send({success: true});
-    })
+      console.log("logging out", user);
+      res.clearCookie('token', {...cookieOptions, maxAge:0}).send({success: true});
+    });
 
     app.get("/queries", async (req, res) => {
       const cursor = queryCollection.find();
@@ -74,8 +95,12 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/queriesByEmail/:email", async (req, res) => {
+    app.get("/queriesByEmail/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
+      if(req.user.email !== email)
+        {
+          return res.status(403).send({message:'forbidden access'})
+        }
     
       // Define the query to search for documents
       const query = { userEmail: email };
